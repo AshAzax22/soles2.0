a44cdf0160f117345a7a6efe0ae4feb51e9cfcac
Ashutosh <ashutoshash.az22@gmail.com>
2024-04-16 18:15:16 +0530

    routing fixes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const uri =
   "mongodb+srv://ashutosh:ashutosh@cluster0.rdfqgfc.mongodb.net/soles";
@@ -15,8 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("./public"));
 
-const port = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "https://soles2-0.vercel.app/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
